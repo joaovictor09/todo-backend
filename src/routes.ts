cb8a4618c1e4b8b8d635fa336dcc1fdb3ab53ecb
João Victor da Silva Cardoso <joaovictordasilva0911@gmail.com>
2023-01-04 00:31:41 -0300

    Create User Route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
+import { CreateUserController } from "./useCase/createUser/CreateUserController";
+
+const createUserController = new CreateUserController();
 
 const router = Router()
 
 router.get("/", (req, res) => { res.send('Hello World') })
+
+router.post('/login' )
+router.post('/signin', createUserController.handle)
 
 export { router }
---
+++ src/useCase/createUser/CreateUserUseCase.ts
@@ -0,0 +1,46 @@
+import { hash } from "bcrypt";
+import { client } from "../../prisma/client";
+
+interface ICreateUser {
+  username: string;
+  password: string;
+}
+
+class CreateUserUseCase{
+  async execute({ username, password }: ICreateUser){
+    if(!username || !password){
+      throw new Error("Username and password are required");
+    }
+    
+    //Verify if user exists
+
+    const userAlreadyExists = await client.user.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if(userAlreadyExists){
+      throw new Error("User already exists")
+    }
+
+    //Hash Password
+
+    const hashPassword = await hash(password, 8)
+
+    //Create user
+
+    const user = await client.user.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    //Return new user
+
+    return user
+  }
+}
+
+export { CreateUserUseCase }
