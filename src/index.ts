47086e6246c468e692226be166c9d401a3617efc
João Victor da Silva Cardoso <joaovictordasilva0911@gmail.com>
2023-01-08 23:18:06 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "express-async-errors"
 import express, { NextFunction, Request, Response } from 'express'
 import { router } from './routes';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(router)
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
